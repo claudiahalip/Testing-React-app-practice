e1a6229297e231f76b9b536dc40923138d196326
claudiahalip <claudia4ro@yahoo.com>
2021-05-10 18:29:24 -0500

    src folder organized

---
+++ src/components/--test--/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+// it is a global functian.First argument is a string description of the test we're running, second argument is a function containing our test logic, stuff to do when this test run 
+it('shows a comment box', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    expect(div.innerHTML).toContain('Comment Box')
+    ReactDOM.unmountComponentAtNode(div);
+})
