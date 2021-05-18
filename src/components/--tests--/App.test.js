2c433927a34024f8a928ec3f24330327caa98eb7
claudiahalip <claudia4ro@yahoo.com>
2021-05-17 21:31:20 -0500

    test for CommentBox component

--- src/components/--tests--/App.test.js
+++ src/components/--tests--/App.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
-
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+let wrapped
+beforeEach(() => {
+   wrapped = shallow(<App />);
+})
 // it is a global functian.First argument is a string description of the test we're running, second argument is a function containing our test logic, stuff to do when this test run 
 it('shows a comment box', () => {
-   const wrapped = shallow(<App />);
    expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it('show a comment list', () =>{
-const wrapped = shallow(<App/>);
-expect(wrapped.find(CommentList).length).toEqual(1)
+   expect(wrapped.find(CommentList).length).toEqual(1)
 })
