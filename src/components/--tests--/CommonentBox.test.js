ffae82ea2e755a362a936f9be118c6fa8e82d7a2
claudiahalip <claudia4ro@yahoo.com>
2021-05-26 14:59:25 -0500

    redux test errors

--- src/components/--tests--/CommonentBox.test.js
+++ src/components/--tests--/CommonentBox.test.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { mount} from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapped;
 
 beforeEach( ()=>{
-    wrapped = mount(<CommentBox/>)
-})
+    wrapped = mount(
+      <Root>
+        <CommentBox/>
+      </Root>
+    );
+});
 
 afterEach(()=>{
     wrapped.unmount();
-})
+});
 
 it('has a textarea and a button', () => {
-     wrapped = mount(<CommentBox />);
     expect(wrapped.find('textarea').length).toEqual(1)
     expect(wrapped.find('button').length).toEqual(1)
 
