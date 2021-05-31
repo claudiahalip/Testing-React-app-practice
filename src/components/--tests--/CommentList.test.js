7583265b96bbe046a0d867fa87f464e10dc89c21
claudiahalip <claudia4ro@yahoo.com>
2021-05-31 13:07:59 -0500

    CommentsList test

--- src/components/--tests--/CommentList.test.js
+++ src/components/--tests--/CommentList.test.js
@@ -5,15 +5,22 @@ import CommentList from 'components/CommentList';
 import Root from 'Root';
 
 let wrapped;
+const initialState={
+    comments: ['Comment1', 'Comment2']
+}
 
 beforeEach(()=>{
     wrapped = mount(
-        <Root>
+        <Root initialState={initialState}>
             <CommentList/>
         </Root>
     )
 })
 
 it('create one LI per comment', ()=>{
-   
+   expect(wrapped.find('li').length).toEqual(2);
+});
+it('show the text for each comment', ()=>{
+    expect(wrapped.render().text('Comment1'))
+    expect(wrapped.render().text('Comment2'))
 })
