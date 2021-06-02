ddbd42d7d8cb52e29f8d08bd86d92d6f9a5e609c
claudiahalip <claudia4ro@yahoo.com>
2021-06-02 08:47:23 -0500

    integration test

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ export function saveComment(comment){
     }
 };
 
-export function fetch_comments (){
+export function fetchComments (){
     const response = axios.get('http://jsonplaceholder.typicode.com/comments')
     return{
         type: FETCH_COMMENTS,
