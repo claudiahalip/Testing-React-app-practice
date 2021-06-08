59455a3a35b3df897c88964bebfa9dd601ec45cf
claudiahalip <claudia4ro@yahoo.com>
2021-06-07 21:52:16 -0500

    render a header

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Link } from 'react-router-dom';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList'
 
 
-export default () => {
+export default class App extends Component {
+    renderButton(){
+    }
+
+    
+    renderheader(){
+        return(
+            <ul>
+                <li>
+                    <Link to= '/'>Home</Link>
+                </li>
+                <li>
+                    <Link to ='/post'>Post a coment</Link>
+                </li>
+                <li>{this.renderButton}</li>
+            </ul>
+        )
+    }
+    render() {
     return (
         <div>
             <Route path ="/post" component= {CommentBox}/>
             <Route path = "/" exact component ={CommentList}/>
         </div>
-    );
+        );
+    }
 };
