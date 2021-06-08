67e4fe0c949bb804fa4d3ae580fb3f7199fb77ac
claudiahalip <claudia4ro@yahoo.com>
2021-06-07 22:04:05 -0500

    sign in, sign out buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import {Route, Link } from 'react-router-dom';
+import {connect} from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList'
 
 
-export default class App extends Component {
+ class App extends Component {
     renderButton(){
+        if(this.props.auth){
+            return<button>Sign Out</button>
+        }else{
+            return <button>Sign In</button>
+        }
     }
 
     
-    renderheader(){
+    renderHeader(){
         return(
             <ul>
                 <li>
@@ -18,16 +24,22 @@ export default class App extends Component {
                 <li>
                     <Link to ='/post'>Post a coment</Link>
                 </li>
-                <li>{this.renderButton}</li>
+                <li>{this.renderButton()}</li>
             </ul>
         )
     }
     render() {
     return (
         <div>
+            {this.renderHeader()}
             <Route path ="/post" component= {CommentBox}/>
             <Route path = "/" exact component ={CommentList}/>
         </div>
         );
     }
 };
+
+function mapStateToProps(state){
+    return {auth: state.auth};
+}
+export default connect(mapStateToProps) (App);
